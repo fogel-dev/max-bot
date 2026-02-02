4771f10c283f30dca6b685f974372a92411920e9
Fogel Egor <e.fogel@cft.ru>
2026-02-02 16:15:19 +0700

    init commit

---
+++ dist/db.js
@@ -0,0 +1,21 @@
+import Database from 'better-sqlite3';
+export const db = new Database('users.db');
+db.prepare(`
+    CREATE TABLE IF NOT EXISTS users (
+                                         user_id TEXT PRIMARY KEY,
+                                         phone TEXT NOT NULL,
+                                         created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+    )
+`).run();
+export function savePhone(userId, phone) {
+    db.prepare(`
+    INSERT INTO users (user_id, phone)
+    VALUES (?, ?)
+    ON CONFLICT(user_id)
+    DO UPDATE SET phone = excluded.phone
+  `).run(userId, phone);
+}
+// функция для проверки содержимого
+export function getAllUsers() {
+    return db.prepare('SELECT * FROM users').all();
+}
---
+++ index.ts
@@ -0,0 +1,37 @@
+import 'dotenv/config';
+import {Bot, Keyboard} from '@maxhub/max-bot-api';
+import {savePhone} from "./db.js";
+
+const bot = new Bot(process.env.BOT_TOKEN!);
+
+
+// Обработчик события запуска бота
+bot.on('bot_started', (ctx) => ctx.reply('👋 Привет! Чтобы продолжить, пожалуйста, поделитесь номером телефона:', {
+    attachments: [
+        Keyboard.inlineKeyboard([
+            [Keyboard.button.requestContact('📱 Отправить номер')],
+        ]),
+    ],
+}));
+
+bot.command('start', (ctx) => ctx.reply('👋 Привет! Чтобы продолжить, пожалуйста, поделитесь номером телефона:', {
+    attachments: [
+        Keyboard.inlineKeyboard([
+            [Keyboard.button.requestContact('📱 Отправить номер')],
+        ]),
+    ],
+}));
+
+
+bot.on('message_created', async (ctx, next) => {
+    if (!ctx.contactInfo) return next();
+    // @ts-ignore
+    const userId = String(ctx.user?.user_id);
+    const phone = String(ctx.contactInfo.tel)
+    console.log('Получен контакт:', userId, phone); // <-- лог в терминал
+    savePhone(userId, phone);
+    await ctx.reply('✅ Ваш номер сохранён!');
+});
+
+bot.start();
+console.log('Бот запущен')
